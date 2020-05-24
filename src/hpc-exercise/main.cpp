3753be7f64cf38ddcaa933d1b4865dc1fc6d05c5
norishigefukushima <fukushima@nitech.ac.jp>
2020-05-24 18:22:14 +0900

    Update main.cpp

--- src/hpc-exercise/main.cpp
+++ src/hpc-exercise/main.cpp
@@ -9,9 +9,21 @@
 
 void inline _mm256_transpose_8x8_ps(__m256* dst, const __m256* src);
 void inline rot(double a, double b, double& x, double& y, double radian);
+void timer_test()
+{
+	CalcTime t;
+	t.start();
+	for (int i = 0; i < 1000; i++)
+	{
+		Sleep(1000);
+		t.end();
+		std::cout << t.getLastTime()/1000 << std::endl;
+	}
+}
 
 int main(const int argc, const char** argv)
 {
+	timer_test();
 	const int loofline_size = 16 * 1024 / sizeof(float);//8KByte
 	const int iteration = 1000000;
 	loofline_test<loofline_size>(iteration, 1);
